20f070da22934bf6b87b4d60fcf74b07df375268
timon384 <pumba@meta.ua>
2019-10-07 15:54:45 +0300

    textures run

--- app/js/script.js
+++ app/js/script.js
@@ -5,12 +5,11 @@ window.onload = function () {
     let canvas = document.getElementById (`canvas`)
     canvas.setAttribute (`width`, width);
     canvas.setAttribute (`height`, height);
-let size_grid = 15;
+let size_grid = 10;
 var controls;
 let matrix = matrixArray(size_grid,size_grid);// генерируем новую матрицу
 let matrix_balls = matrixArray_ball(size_grid,size_grid,width,height); // генерируем новую матрицу balls
 console.log (matrix_balls);
-/*console.log (matrix_balls);*/
 // выбор сетки
 
 // функция создания новой матрицы
@@ -19,7 +18,7 @@ function matrixArray(rows,columns){
   for(var i=0; i<rows; i++){
     arr[i] = new Array();
     for(var j=0; j<columns; j++){
-    n =Math.random();
+    n =Math.random();/*Math.random() вернуть после отладки*/
         if ( n <= 0.8) {
         arr[i][j] = 0;
         } else {arr[i][j] = 1;}
@@ -52,9 +51,9 @@ this.rotationY = rotationY;
 this.rotationZ = rotationZ;
 }
 }
-        let n =  new balls (i*height/rows + j*height/rows/2, j*height/columns + height/columns/2, 100, 0.001, 0.001, 0.001);
+        let n =  new balls (i*height/rows + height/rows/2 - height/2, j*height/columns + height/columns/2 -height/2, height/size_grid/2.5, 0.001, 0.001, 0.001);
         arr[i][j] = n;
-        } else {arr[i][j] = 0;}
+        } else{arr[i][j] = 0;}
 
     }
   }
@@ -66,38 +65,45 @@ this.rotationZ = rotationZ;
 // создание сцены
     let scene = new THREE.Scene();
 // создание камеры
-    let camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 5000);
-    camera.position.set (0, -1200, 450);
+    let camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 10000);
+    camera.position.set (0, 0, 1000);
     camera.lookAt( scene.position );
 
 // создание света
-    let light = THREE.AmbientLight (0xffffff);
+    var light = new THREE.AmbientLight (0xffffff);
     scene.add(light); // добавляем свет в сцену
+
 // прокрутка мышью
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
+scene.add( line );
+    let texture_ball = THREE.ImageUtils.loadTexture('app/static/images/ameba_green.jpg'); //определяем текстуру шара
+    let texture_board = THREE.ImageUtils.loadTexture('app/static/images/stone.jpg'); //определяем текстуру подложки
 
-    let ball = new THREE.SphereGeometry (20, 12, 12);
-    let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true}); // создание материала
+    // создание объектов
+    let ball = new THREE.SphereGeometry (height/size_grid/2.5, 12, 12); // создание шарика
+    let line = new THREE.Line( geometry, material );
+    let material_ball = new THREE.MeshLambertMaterial( {map: texture_ball} ); // создание материала
+    let material_board = new THREE.MeshLambertMaterial( {map: texture_board} ); // создание материала
+//   Создание текстуры
 
 for(var i=0; i<size_grid; i++){
     for(var j=0; j<size_grid; j++){
         if ( matrix_balls[i][j] !== 0) {
-    let mesh = new THREE.Mesh (ball, material); // создание сетки для фигуры
+    let mesh = new THREE.Mesh (ball, material_ball); // создание сетки для фигуры
     mesh.position.x = matrix_balls[i][j].positionX;
     mesh.position.y = matrix_balls[i][j].positionY;
     mesh.position.z = matrix_balls[i][j].positionZ;
     scene.add (mesh);
         } else {}
-
     }
   }
 
     let geometry = new THREE.PlaneGeometry( height, height, size_grid, size_grid); // создание геометрической фигуры
-    let mesh = new THREE.Mesh (geometry, material);
+    let mesh = new THREE.Mesh (geometry, material_board);
     mesh.position.x = 0;
     mesh.position.y = 0;
-    mesh.position.z = -20;
+    mesh.position.z = 0;
     scene.add (mesh);
 
 // создаем движение
